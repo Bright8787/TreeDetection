cd3f2b812cc885111a305521329aa669404aedd3
bright8787 <phurin8787@gmail.com>
2025-10-30 21:20:39 +0100

    app mit doppel klick fix

---
+++ src/app_src/run_me.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VENV_PYTHON="/home/bright/testenv/bin/python"
+GUI_SCRIPT="/home/bright/TreeDetectionRepo/TreeDetection/src/app_src/python-gui.py"
+"$VENV_PYTHON" -u "$GUI_SCRIPT" 
+
+read -p "SKript beendet.."
